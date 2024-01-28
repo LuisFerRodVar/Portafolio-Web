fb09db553c10483dd5026de3a20b568b323e2cc3
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-01-28 10:55:49 -0600

    Cambio ligero de prueba

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById('formBtn');
+var form = document.getElementById ("form");
+var formData = new FormData (form);
 
 document.getElementById('form')
   .addEventListener('submit', function(event) {
@@ -6,15 +8,16 @@ document.getElementById('form')
 
     btn.value = 'Enviando...';
 
-    const serviceID = 'default_service';
-    const templateID = 'template_yms435s';
 
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Enviado';
-      }, (err) => {
-        btn.value = 'Error';
-      });
 
+    Email.send({
+      SecureToken : "e810590b-d480-4f9e-87e1-2111667cb4a0",
+      To : 'luisferrodvar@gmail.com',
+      From : "portafoliowebb@gmail.com",
+      Subject : formData.get('subject') + formData.get('name'),
+      Body : formData.get('message') + 'email: ' + formData.get('email')
+  }).then(
+    message =>  btn.value = 'Enviado'
+  );
     return false; // Agregar esta línea para evitar la recarga de la página
   });
