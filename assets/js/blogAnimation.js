e9b1da789f2c460a78a0ea30d4cd9ff61e5da4cb
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-02-02 20:05:31 -0600

    Cambios en el menú de navegación, animación en la sección de blog y el contenido del primer blog

---
+++ assets/js/blogAnimation.js
@@ -0,0 +1,40 @@
+function showMask(element, destiny) {
+    // Obtener la máscara y layout__content
+    const mask = document.getElementById('mask');
+    const contentBlog = document.querySelector('.content__blog');
+    const articles = document.querySelectorAll('.articles__article');
+    const title = document.querySelector('.blog__header');
+    const menu = document.querySelector('.menu__list');
+    const options = document.querySelectorAll('.menu__option i')
+
+    
+    const rect = element.getBoundingClientRect();
+    const contentRect = contentBlog.getBoundingClientRect();
+    const x = rect.left - contentRect.left + rect.width / 2;
+    const y = rect.top - contentRect.top + rect.height / 2;
+
+    title.style.opacity = '0';
+    mask.style.left = x + 'px';
+    mask.style.top = y + 'px';
+
+    // Ocultar el resto de elementos dentro de .content__page
+    articles.forEach(article => {
+        article.style.opacity = '0';
+    });
+    // Ajustar la máscara al tamaño inicial (cero)
+    mask.style.width = '0';
+    mask.style.height = '0';
+
+    // Mostrar y animar la máscara al tamaño de .content__blog
+    mask.style.width = "250%";
+    mask.style.height = "250%";
+    if(destiny == 1){
+
+
+        
+        setTimeout(() =>{
+            
+            window.location.href = 'prueba.html'
+        },1000)
+    }
+}
